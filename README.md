6b08f2fb05f9098342f0c9f98d8198fecbad8082
edge <sort@programmer.net>
2015-03-01 06:18:34 +0900

    implement self closing tags

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # prim
-Fat free and extensible interface markup for the web.
 
-Pure Recursive Interface Markup
+## Pure Recursive Interface Markup
+
+Natural, fat free, and extensible interface markup for the web.
+
+Prim is a new templating language that implements only the most intuitive and natural concepts of interface structure, allowing for developers to efficiently create platform-agnostic user interfaces. It leaves out specific rules like `img tags are self closing` to achieve maximum performance and transparency. The simple, unopinionated design of Prim allows for a healthy balance between recyclability and output certainty.
+
+Prim is great for creating web applications. The parser returns a plain HTML string that can be treated as a reusable atomic DOM node. It plays well with client-side MVC frameworks such as AngularJS and Backbone, and servers like Express (Node.js).
+
+Prim may be implemented in the future as an interface language for software beyond web applications.
+
+## Browser
+
+In your HTML, link to your Prim library:
+```html
+<script src='/js/prim.js'></script>
+```
+
+Now you can convert Prim to HTML:
+```js
+prim.parse('div(class="article") { p { "Lorem ipsum dolor sit amet" } }');
+// "<div class="article"><p>Lorem ipsum dolor sit amet</p></div>"
+```
+
+## Server
+
+Coming soon.
--- spec/prim_spec.js
+++ spec/prim_spec.js
@@ -4,68 +4,98 @@
 
 describe('prim', function() {
 	describe('parse', function() {
-		it('takes empty values', function() {
-			expect(prim.parse('').toString()).toBe('');
+		it('parses mixed strings and tags', function() {
+			expect(prim.parse('div "Hello" img/')).toBe('<div></div>Hello<img />');
 		});
 
-		it('parses a simple tag', function() {
-			expect(prim.parse('html')).toBe('<html></html>');
+		it('parses strings inside tags', function() {
+			expect(prim.parse('div { "Hello" }')).toBe('<div>Hello</div>');
 		});
 
-		it('parses a nested tag', function() {
-			expect(prim.parse('html { body }')).toBe('<html><body></body></html>');
+		it('parses plain strings', function() {
+			expect(prim.parse('"Hello"')).toBe('Hello');
 		});
 
-		it('parses a double nested tag', function() {
-			expect(prim.parse('html { body { div } }')).toBe('<html><body><div></div></body></html>');
+		it('parses mixed self closing and nested tags', function() {
+			expect(prim.parse('img(src="/img/logo.png")/ div { img(src="/img/logo.png")/ } img/img/')).toBe('<img src="/img/logo.png" /><div><img src="/img/logo.png" /></div><img /><img />');
 		});
 
-		it('parses two tags', function() {
-			expect(prim.parse('div div')).toBe('<div></div><div></div>');
+		it('parses attributes with solidi on self closing tags', function() {
+			expect(prim.parse('img(src="/img/logo.png")/')).toBe('<img src="/img/logo.png" />');
 		});
 
-		it('parses three distinct tags', function() {
-			expect(prim.parse('div i span')).toBe('<div></div><i></i><span></span>');
+		it('cannot parse tags starting with solidi', function() {
+			expect(function() {
+				prim.parse('/div');
+			}).toThrow();
 		});
 
-		it('parses mixed nested and distinct tags', function() {
-			expect(prim.parse('div { span } i span')).toBe('<div><span></span></div><i></i><span></span>');
+		it('parses self closing tags', function() {
+			expect(prim.parse('img/')).toBe('<img />');
 		});
 
-		it('parses mixed doubled nested and distinct tags', function() {
-			expect(prim.parse('div { span div { hr }} i span { u b { span } }')).toBe('<div><span></span><div><hr></hr></div></div><i></i><span><u></u><b><span></span></b></span>');
+		it('parses multiple mixed attribute lists', function() {
+			expect(prim.parse('div(attr="value" value="text" checked href="#" focused)')).toBe('<div attr="value" value="text" checked href="#" focused></div>');
+		});
+
+		it('parses single boolean attribute lists', function() {
+			expect(prim.parse('div(attr)')).toBe('<div attr></div>');
+		});
+
+		it('parses multiple attribute lists', function() {
+			expect(prim.parse('div(attr="value" value="text" href="#")')).toBe('<div attr="value" value="text" href="#"></div>');
+		});
+
+		it('parses single attribute lists', function() {
+			expect(prim.parse('div(attr="value")')).toBe('<div attr="value"></div>');
+		});
+
+		it('parses empty attribute lists', function() {
+			expect(prim.parse('div()')).toBe('<div></div>');
+		});
+
+		it('parses newlines between braces', function() {
+			expect(prim.parse('html {\n\n}')).toBe('<html></html>');
+		});
+
+		it('parses empty nested tag', function() {
+			expect(prim.parse('html {}')).toBe('<html></html>');
 		});
 
 		it('parses mixed doubled nested and distinct tags with mimimal spaces', function() {
 			expect(prim.parse('div{span div{hr}}i span{u b{span}}')).toBe('<div><span></span><div><hr></hr></div></div><i></i><span><u></u><b><span></span></b></span>');
 		});
 
-		it('parses empty nested tag', function() {
-			expect(prim.parse('html {}').toString()).toBe('<html></html>');
+		it('parses mixed doubled nested and distinct tags', function() {
+			expect(prim.parse('div { span div { hr }} i span { u b { span } }')).toBe('<div><span></span><div><hr></hr></div></div><i></i><span><u></u><b><span></span></b></span>');
 		});
 
-		it('parses newlines between braces', function() {
-			expect(prim.parse('html {\n\n}').toString()).toBe('<html></html>');
+		it('parses mixed nested and distinct tags', function() {
+			expect(prim.parse('div { span } i span')).toBe('<div><span></span></div><i></i><span></span>');
 		});
 
-		it('parses empty attribute lists', function() {
-			expect(prim.parse('div()').toString()).toBe('<div></div>');
+		it('parses three distinct tags', function() {
+			expect(prim.parse('div i span')).toBe('<div></div><i></i><span></span>');
 		});
 
-		it('parses single attribute lists', function() {
-			expect(prim.parse('div(attr="value")').toString()).toBe('<div attr="value"></div>');
+		it('parses two tags', function() {
+			expect(prim.parse('div div')).toBe('<div></div><div></div>');
 		});
 
-		it('parses multiple attribute lists', function() {
-			expect(prim.parse('div(attr="value" value="text" href="#")').toString()).toBe('<div attr="value" value="text" href="#"></div>');
+		it('parses a double nested tag', function() {
+			expect(prim.parse('html { body { div } }')).toBe('<html><body><div></div></body></html>');
 		});
 
-		it('parses single boolean attribute lists', function() {
-			expect(prim.parse('div(attr)').toString()).toBe('<div attr></div>');
+		it('parses a nested tag', function() {
+			expect(prim.parse('html { body }')).toBe('<html><body></body></html>');
 		});
 
-		it('parses multiple mixed attribute lists', function() {
-			expect(prim.parse('div(attr="value" value="text" checked href="#" focused)').toString()).toBe('<div attr="value" value="text" checked href="#" focused></div>');
+		it('parses a simple tag', function() {
+			expect(prim.parse('html')).toBe('<html></html>');
+		});
+
+		it('takes empty values', function() {
+			expect(prim.parse('')).toBe('');
 		});
 	});
 });

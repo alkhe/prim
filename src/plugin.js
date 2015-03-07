14fe8ff000dbe9230d25f9cbb693b9f6620ab50c
edge <sort@programmer.net>
2015-03-07 15:31:23 +0900

    compile and make small fixes

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,8 @@
 module.exports.use = function(config) {
 	var parse = config.parser.parse;
 	config.parser.parse = function(input, context) {
-		return parse(input, {}, context);
+		return (function() {
+			return parse(input, {}, context);
+		})();
 	};
 }
